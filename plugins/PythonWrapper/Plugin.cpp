968ea914e3b4c28f51f157e21eb25577400d709d
Owerosu <owerosu@yahoo.fr>
2019-02-03 23:09:57 +0100

    Add basic handling of python commands (2 exported APIs)

---
+++ plugins/PythonWrapper/Plugin.cpp
@@ -0,0 +1,166 @@
+#include <Python.h> // Must be" first, see https://stackoverflow.com/questions/15078060/embedding-python-in-qt-5
+#include "Plugin.h"
+#include "edb.h"
+#include <QMessageBox>
+#include <QMenu>
+#include <QDebug>
+#include <QEventLoop>
+#include <QMainWindow>
+#include <QDockWidget>
+#include <QPlainTextEdit>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
+#include <pybind11/eval.h>
+#include <string>
+namespace PythonWrapper {
+
+QString helperColoredString(QString color, QString msg)
+{
+	QString coloredText ;
+	coloredText.append("<font color=\"");
+	coloredText.append(color);
+	coloredText.append("\">");
+	coloredText.append(msg);
+	coloredText.append("</font>");
+	return coloredText;
+}
+
+PythonWrapper* instance = nullptr;
+
+bool IS_WAITING_TEXT_INPUT = false;
+
+PYBIND11_EMBEDDED_MODULE(bridge, m) {
+    // `m` is a `py::module` which is used to bind functions and classes
+    m.def("print", [](std::string str) {
+    	instance->bridgePrint(str);
+    });
+    m.def("input", []() {
+        QEventLoop loop;
+        QObject::connect(instance->lineWidget(), SIGNAL(returnPressed()), &loop, SLOT(quit()));
+        IS_WAITING_TEXT_INPUT = true;
+        loop.exec();
+        auto ligne = instance->lineWidget()->text().toStdString();
+        instance->bridgePrintInput(ligne);
+        instance->lineWidget()->setText("");
+        IS_WAITING_TEXT_INPUT = false;
+        return ligne;
+    });
+
+}
+PYBIND11_EMBEDDED_MODULE(edb, m) {
+    m.def("get_expression_from_user", [](std::string title,std::string msg) {
+    	edb::address_t  value;
+    	edb::v1::get_expression_from_user(QString::fromStdString(title),QString::fromStdString(msg),&value);
+    	return (unsigned long)value;
+    });
+    m.def("dump_data", [](unsigned long addr) {
+    	edb::v1::dump_data(addr);
+    	return;
+    });
+}
+// Skeleton taken from DebuggerErrorConsole plugin
+PythonWrapper::PythonWrapper() : menu_(nullptr) {
+	//Py_Initialize();
+	instance = this;
+	textWidget_ = new QPlainTextEdit;
+	textWidget_->setReadOnly(true);
+	QFont font("monospace");
+	font.setStyleHint(QFont::TypeWriter);
+	textWidget_->setFont(font);
+	textWidget_->setWordWrapMode(QTextOption::WrapMode::NoWrap);
+	textWidget_->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+	//qInstallMessageHandler(debugMessageIntercept);
+
+	lineWidget_ = new QLineEdit;
+	lineWidget_->setFont(font);
+    connect(lineWidget_, SIGNAL(returnPressed()),this, SLOT(lineSent())); // FIXME: Works only with numpad key as the event is caught somewhere else
+}
+
+PythonWrapper::~PythonWrapper() {
+}
+QLineEdit* PythonWrapper::lineWidget(){
+	return lineWidget_;
+}
+QMenu *PythonWrapper::menu(QWidget *parent) {
+	if(!menu_) {
+		interpreter_ = new pybind11::scoped_interpreter;
+		std::string stdOutErr = // inspired from https://stackoverflow.com/questions/46632488/how-to-catch-python-3-stdout-in-c-code
+	    "import bridge\n"
+		"def _print(txt):\n"
+		"    bridge.print(str(txt))\n"
+		"def _input(txt=None):\n"
+		"    if txt:\n"
+		"        bridge.print(str(txt))\n"
+		"    return bridge.input()\n"
+		"print=_print\n"
+		"input=_input\n";
+		pybind11::exec(stdOutErr);
+		if(auto mainWindow = qobject_cast<QMainWindow *>(edb::v1::debugger_ui)) {
+			auto dockWidget = new QDockWidget(tr("Python console"), mainWindow);
+			dockWidget->setObjectName(QString::fromUtf8("Python console"));
+			auto layout = new QVBoxLayout;
+			auto widget = new QWidget;
+			layout->addWidget(textWidget_);
+			layout->addWidget(lineWidget_);
+			widget->setLayout(layout);
+			dockWidget->setWidget(widget);
+
+			mainWindow->addDockWidget(Qt::BottomDockWidgetArea, dockWidget);
+
+			menu_ = new QMenu(tr("Python Script"), parent);
+			menu_->addAction(dockWidget->toggleViewAction());
+
+			auto docks = mainWindow->findChildren<QDockWidget *>();
+
+			// We want to put it to the same area as Stack dock
+			// Stupid QList doesn't have a reverse iterator
+			for(auto it = docks.end() - 1 ;; --it) {
+				QDockWidget *const widget = *it;
+
+				if(widget != dockWidget && mainWindow->dockWidgetArea(widget) == Qt::BottomDockWidgetArea) {
+					mainWindow->tabifyDockWidget(widget, dockWidget);
+					widget->show();
+					widget->raise();
+					break;
+				}
+
+				if(it == docks.begin()) {
+					break;
+				}
+			}
+
+		}
+	}
+	return menu_;
+}
+void PythonWrapper::bridgePrint(std::string str){
+	textWidget_->appendPlainText(QString::fromStdString(str));
+}
+/* Display value expected by input() in another color for visibility */
+void PythonWrapper::bridgePrintInput(std::string str){
+	textWidget_->appendHtml(helperColoredString(QString("blue"),QString::fromStdString(str)));
+}
+void PythonWrapper::lineSent() {
+	if(!IS_WAITING_TEXT_INPUT)
+	{
+		auto command = lineWidget_->text();
+		textWidget_->appendPlainText(">>> "+command);
+		lineWidget_->setText("");
+		try{
+			pybind11::exec(command.toStdString());
+		}
+		catch(pybind11::error_already_set &e)
+		{
+			textWidget_->appendHtml(helperColoredString(QString("red"),e.what()));
+		}
+	}
+}
+
+void PythonWrapper::runScript() {
+    QMessageBox::information(0, tr("Not implemented"), tr("Not implemented"));
+}
+
+}
---
+++ plugins/PythonWrapper/Plugin.h
@@ -0,0 +1,46 @@
+#ifndef MYPLUGIN_H_
+#define MYPLUGIN_H_
+#include <pybind11/embed.h>
+#include "IPlugin.h"
+
+
+class QPlainTextEdit;
+class QLineEdit;
+
+// we wrap everything in a namespace, this way we don't conflict with
+// symbols from other plugins or edb itself
+namespace PythonWrapper {
+
+// inherit from both QObject and IPlugin
+class PythonWrapper : public QObject, public IPlugin {
+    // some requisite meta-data
+    Q_OBJECT
+    Q_INTERFACES(IPlugin)
+#if QT_VERSION >= 0x050000
+    Q_PLUGIN_METADATA(IID "edb.IPlugin/1.0")
+#endif
+    Q_CLASSINFO("author", "Owerosu")
+    Q_CLASSINFO("url", "https://github.com/owerosu/")
+
+public:
+    PythonWrapper();
+    virtual ~PythonWrapper() override;
+    void bridgePrint(std::string str);
+    void bridgePrintInput(std::string str);
+    QLineEdit* lineWidget();
+public:
+    virtual QMenu *menu(QWidget *parent = nullptr) override;
+public Q_SLOTS:
+    void runScript();
+    void lineSent();
+
+private:
+    QMenu *menu_ = nullptr;
+    QPlainTextEdit* textWidget_ = nullptr;
+    QLineEdit* lineWidget_ = nullptr;
+	pybind11::scoped_interpreter *interpreter_ = nullptr;
+};
+
+}
+
+#endif
