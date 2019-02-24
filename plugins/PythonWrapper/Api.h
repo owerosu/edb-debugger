a0abf378e84b850b1c2278f14140f7047a1e71c5
Owerosu <owerosu@yahoo.fr>
2019-02-24 18:58:18 +0100

    Add Symbol Manager to python API

--- plugins/PythonWrapper/Api.h
+++ plugins/PythonWrapper/Api.h
@@ -9,12 +9,13 @@
 #define PLUGINS_PYTHONWRAPPER_API_H_
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h>
+#include <pybind11/stl.h>
 #include "edb.h"
 #include "ISymbolManager.h"
 #include "Symbol.h"
-
-
+#include <QStringList>
 namespace py = pybind11;
+
 
 
 PYBIND11_EMBEDDED_MODULE(edbv1, m) {
@@ -23,14 +24,9 @@ PYBIND11_EMBEDDED_MODULE(edbv1, m) {
     	edb::v1::get_expression_from_user(QString::fromStdString(title),QString::fromStdString(msg),&value);
     	return value;
     });
-    m.def("dump_data", [](edb::address_t addr) {
-    	edb::v1::dump_data(addr);
-    	return;
-    });
-    m.def("dump_data", [](edb::address_t addr,bool new_tab) {
-    	edb::v1::dump_data(addr,new_tab);
-    	return;
-    });
+    m.def("dump_data", (bool (*)(edb::address_t)) &edb::v1::dump_data);
+    m.def("dump_data", (bool (*)(edb::address_t,bool)) &edb::v1::dump_data);
+
     py::class_<edb::address_t>(m, "address_t")
     	.def(py::init<unsigned long>())
 		.def(py::init<edb::address_t>())
@@ -164,11 +160,62 @@ PYBIND11_EMBEDDED_MODULE(edbv1, m) {
 			},py::is_operator());
 
     py::class_<ISymbolManager>(m, "symbol_manager")
-    	.def_static("test",
-    		[](){
-    	 	 	 return 42;
-    		});
-    py::class_<Symbol>(m, "Symbol")
+		.def_static("symbols",
+			[](){
+				 return edb::v1::symbol_manager().symbols().toStdList();
+			})
+		.def_static("find",
+					[](const edb::address_t address){
+						 return edb::v1::symbol_manager().find(address);
+					})
+		.def_static("find",
+					[](const std::string str){
+						 return edb::v1::symbol_manager().find(QString::fromStdString(str));
+					})
+		.def_static("find_near_symbol",
+					[](const edb::address_t address){
+						 return edb::v1::symbol_manager().find_near_symbol(address);
+					})
+		.def_static("add_symbol",
+					[](const std::shared_ptr<Symbol> address){
+						 edb::v1::symbol_manager().add_symbol(address);
+					})
+		.def_static("clear",
+					[](){
+						 edb::v1::symbol_manager().clear();
+					})
+		.def_static("load_symbol_file",
+					[](const std::string str,const edb::address_t address){
+						 edb::v1::symbol_manager().load_symbol_file(QString::fromStdString(str),address);
+					})
+		.def_static("set_label",
+					[](const edb::address_t address,const std::string str){
+						 edb::v1::symbol_manager().set_label(address,QString::fromStdString(str));
+					})
+		.def_static("find_address_name",
+					[](const edb::address_t address,bool prefixed){
+						 return edb::v1::symbol_manager().find_address_name(address,prefixed).toStdString();
+					},py::arg("address"),py::arg("prefixed") = true)
+					/*
+					 * TODO:
+		.def_static("set_symbol_generator",
+					[](){
+						 return edb::v1::symbol_manager().set_symbol_generator(gen);
+					})
+					+ QHash
+					*/
+		.def_static("files",
+					[](){
+    					 std::list<std::string> list ;
+    					 QStringList::const_iterator el;
+    					 for(el = edb::v1::symbol_manager().files().constBegin(); el != edb::v1::symbol_manager().files().constEnd(); ++el)
+						 {
+    						 list.push_back((*el).toStdString());
+						 }
+						 return list;
+					});
+
+    py::class_<Symbol,std::shared_ptr<Symbol>>(m, "Symbol")
 		.def(py::init())
 		.def_property("file",
 			[](const Symbol &sym){
